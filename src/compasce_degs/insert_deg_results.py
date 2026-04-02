9f8c8b181e4d1a7942f18607e099af701717c0dc
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-04-02 10:00:28 -0400

    Handle empty df

--- src/compasce_degs/insert_deg_results.py
+++ src/compasce_degs/insert_deg_results.py
@@ -16,7 +16,9 @@ def insert_celltype_vs_rest_degs(
     if ladata.has_zdone(["uns", out_key]):
         return ladata
     
-    # TODO: what to do when df is empty (insufficient data for DEG analysis)?
+    # Check whether df is empty (insufficient data for DEG analysis).
+    df = pd.read_csv(csv_path)
+    is_empty = df.shape[0] == 0
 
     cmp = cm.add_comparison([("compare", cell_type_col), ("val", cell_type_name), "__rest__"])
 
@@ -26,15 +28,15 @@ def insert_celltype_vs_rest_degs(
             "baseline": "rest",
             "group_to_compare": cell_type_name,
         },
+        "is_empty": is_empty,
     }, {
         "obsType": "cell",
         "featureType": "gene",
         "obsSetSelection": [[cell_type_col, cell_type_name]],
     })
 
-    df = pd.read_csv(csv_path)
-
-    ladata.uns[uns_key] = df
+    if not is_empty:
+        ladata.uns[uns_key] = df
 
     # Write a .zdone file
     ladata.write_zdone(["uns", out_key])
@@ -57,7 +59,9 @@ def insert_within_celltype_case_vs_control_degs(
     if ladata.has_zdone(["uns", out_key]):
         return ladata
     
-    # TODO: what to do when df is empty (insufficient data for DEG analysis)?
+    # Check whether df is empty (insufficient data for DEG analysis).
+    df = pd.read_csv(csv_path)
+    is_empty = df.shape[0] == 0
 
     cmp = cm.add_comparison([("filter", cell_type_col), ("val", cell_type_name), ("compare", sample_group_col), ("val", sample_group_lhs), ("val", sample_group_rhs)])
 
@@ -67,6 +71,7 @@ def insert_within_celltype_case_vs_control_degs(
             "baseline": f"{cell_type_name}_{sample_group_lhs}",
             "group_to_compare": f"{cell_type_name}_{sample_group_rhs}",
         },
+        "is_empty": is_empty,
     }, {
         "obsType": "cell",
         "featureType": "gene",
@@ -75,9 +80,8 @@ def insert_within_celltype_case_vs_control_degs(
         "sampleSetFilter": [[sample_group_col, sample_group_lhs], [sample_group_col, sample_group_rhs]],
     })
 
-    df = pd.read_csv(csv_path)
-
-    ladata.uns[uns_key] = df
+    if not is_empty:
+        ladata.uns[uns_key] = df
 
     # Write a .zdone file
     ladata.write_zdone(["uns", out_key])
